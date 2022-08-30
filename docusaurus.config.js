ad586d78bd98baf83337fb816fdf6e9374225497
Mehmet Kırçal <Mkircal957@gmail.com>
2022-08-30 17:14:34 +0300

    s

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,9 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Notus Network Docs",
-  tagline: "Notus Network Docs",
+  title: "Notus Docs",
+  tagline:
+    "Notus is an open source project implementing a new, high-performance, permissionless blockchain.",
   url: "http://docs.notus.network",
   baseUrl: "/",
   onBrokenLinks: "throw",
@@ -42,104 +43,102 @@ const config = {
       }),
     ],
   ],
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "Notus Network Docs",
-        logo: {
-          alt: "Site Logo",
-          src: "img/brand/Notus_icon_gradient_bg.svg",
-        },
-        items: [
-          {
-            type: "doc",
-            docId: "Overview/intro",
-            position: "left",
-            label: "Overview",
-          },
-          {
-            type: "doc",
-            docId: "Node/intro",
-            position: "left",
-            label: "Node",
-          },
-          {
-            type: "doc",
-            docId: "Developer/intro",
-            position: "left",
-            label: "Developer",
-          },
-          {
-            type: "doc",
-            docId: "Api/REST/intro",
-            position: "left",
-            label: "API",
-          },
-          {
-            type: "doc",
-            docId: "Advanced/Oracle",
-            position: "left",
-            label: "Advanced",
-          },
-          {
-            type: "doc",
-            docId: "Migration/index",
-            position: "left",
-            label: "Migration",
-          },
-          {
-            type: "doc",
-            docId: "FAQ/index",
-            position: "left",
-            label: "FAQ",
-          },
-          {
-            href: "https://github.com/Notus-Network",
-            label: "GitHub",
-            position: "right",
-          },
-          {
-            type: "localeDropdown",
-            position: "right",
-          },
-        ],
+  themeConfig: {
+    navbar: {
+      logo: {
+        alt: "Notus Logo",
+        src: "img/brand/logo/white.svg",
+        srcDark: "img/brand/logo/dark.svg",
       },
-      /* algolia: {
+      items: [
+        {
+          type: "doc",
+          docId: "Overview/intro",
+          position: "left",
+          label: "Overview",
+        },
+        {
+          type: "doc",
+          docId: "Node/intro",
+          position: "left",
+          label: "Node",
+        },
+        {
+          type: "doc",
+          docId: "Developer/intro",
+          position: "left",
+          label: "Developer",
+        },
+        {
+          type: "doc",
+          docId: "Api/REST/intro",
+          position: "left",
+          label: "API",
+        },
+        {
+          type: "doc",
+          docId: "Advanced/Oracle",
+          position: "left",
+          label: "Advanced",
+        },
+        {
+          type: "doc",
+          docId: "Migration/index",
+          position: "left",
+          label: "Migration",
+        },
+        {
+          type: "doc",
+          docId: "FAQ/index",
+          position: "left",
+          label: "FAQ",
+        },
+        {
+          href: "https://github.com/Notus-Network",
+          label: "GitHub",
+          position: "right",
+        },
+        {
+          type: "localeDropdown",
+          position: "right",
+        },
+      ],
+    },
+    /* algolia: {
         appId: 'LRR0WHMG8Y',
         apiKey: '80436f7efbeccb8701d266197aaafad8',
         indexName: '/',
       },*/
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Docs",
-                to: "/",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "GitHub",
-                href: "https://github.com/Notus-Network",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Notus Network`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ["csharp", "typescript"],
-      },
-    }),
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Docs",
+              to: "/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/Notus-Network",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Notus Network`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+      additionalLanguages: ["csharp", "typescript"],
+    },
+  },
 };
 
 module.exports = config;
